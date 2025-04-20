7dd00e6f14fa4e8658e00b3f10f1320ae101ff0b
Joaquin <JoaquinJSX>
2025-04-20 09:56:12 -0500

    Creating dist file

--- Frontend/Components/ProjectCard.tsx
+++ Frontend/Components/ProjectCard.tsx
@@ -10,7 +10,7 @@ interface Props {
 export default function Card({ name, image, tools, link }: Props) {
 
     return (
-        <div>
+        <div className={styles.ProjectCard}>
             <h3>
                 {/*Nombre del projecto*/}
                 {name}
